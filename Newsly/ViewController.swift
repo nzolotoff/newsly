d37d5a3e1d8b3a1ef53a620915e86247283059a5
nzolotoff <nikita.zolotov37@yandex.ru>
2025-03-01 11:06:51 +0300

    initial

---
+++ Newsly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newsly
+//
+//  Created by Nikita Zolotov on 01.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
